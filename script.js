3695445748fea873a0061622f3d4e957732a3101
Adya Bharatdwaj <adyabharatdwaj@gmail.com>
2026-07-18 13:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const price = document.getElementsByClassName("price")
+	let sum = 0
+	for(let i of price)
+		{
+			sum += Number(i.textContent)
+		}
+
+	const tr = document.createElement("tr");
+	const td = document.createElement("td")
+	
+	td.colSpan = 2;
+	td.textContent = sum;
+
+	const table = document.querySelector("table");
+	
+	tr.appendChild(td);
+	table.appendChild(tr);
   
 };
 
