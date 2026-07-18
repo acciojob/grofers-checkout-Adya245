abdf3db5c4da56e5696f2628324f250a7bbdc38e
Adya Bharatdwaj <adyabharatdwaj@gmail.com>
2026-07-18 13:07:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,26 +3,30 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-	const price = document.getElementsByClassName("price")
-	let sum = 0
-	for(let i of price)
-		{
-			sum += Number(i.textContent)
-		}
+    const price = document.getElementsByClassName("price");
+    let sum = 0;
 
-	const tr = document.createElement("tr");
-	const td = document.createElement("td")
-	
-	td.colSpan = 2;
-	td.textContent = sum;
+    for (let i of price) {
+        sum += Number(i.textContent);
+    }
 
-	const table = document.querySelector("table");
-	
-	tr.appendChild(td);
-	table.appendChild(tr);
-  
+    const table = document.querySelector("table");
+
+    // Remove previous total row if it exists
+    const oldRow = document.getElementById("total-row");
+    if (oldRow) {
+        oldRow.remove();
+    }
+
+    const tr = document.createElement("tr");
+    tr.id = "total-row";
+
+    const td = document.createElement("td");
+    td.colSpan = 2;
+    td.textContent = sum;
+
+    tr.appendChild(td);
+    table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
